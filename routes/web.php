78f49964652547b3fe350c808d91990e2fa43c1f
= <=>
2020-06-08 16:50:32 +0300

    'xxxxx'

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 
-Auth::routes();
+Auth::routes(['verify' => true]);
+//Route::get('/verify', 'Auth\VerificationController@show')->name('verify');
 
 Route::get('/', 'QuizController@index')->name('home');
 Route::post('/', 'QuizController@store');
